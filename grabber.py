329477fc169ed237851aa962cc3c5a7aa33c6256
Nootan Ghimire <nootan.ghimire@gmail.com>
2013-07-22 02:45:21 -0700

    Removed useless comments.

--- grabber.py
+++ grabber.py
@@ -5,10 +5,7 @@
 # Rather than a exploit, it is a service given by fb :D
 #######################################################################
 
-
-#import urllib request
 import urllib.request
-#import urllib error handling
 from urllib.error import HTTPError,URLError
 
 #function that downloads a file
@@ -23,7 +20,6 @@ def downloadFile(file_name,file_mode,base_url):
 
             # Open our local file for writing
             local_file = open(file_name+".jpg", "w" + file_mode)
-            #Write to our local file
             local_file.write(f.read())
             local_file.close()
 
@@ -33,7 +29,7 @@ def downloadFile(file_name,file_mode,base_url):
     except URLError as e:
             print("URL Error:",e.reason , url)
 
-#Function Ends !!
+#End Function
             
 profile=100000425733973 #Start From Profile ID          
 
@@ -46,15 +42,4 @@ while(1==1):
     downloadFile(file_name,"b",base_url)
     profile=profile+1
 #End While Loop
-
-# TODO : This program grabs profile pictures by
-# bruteforcing the profile id's. So, if a person's
-# profile id does not match, instead of returning
-# nothing, facebook returns default profile picture
-# So it would be good to overcome this,
-# by a image manipulating library which deletes those
-# default images
-
-
-#################################################################
-#End Program#
+#End Program
