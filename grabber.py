a4103db3c4f111f9fcf537d0b4a64fbcc79378d8
Amit <studenton.com@gmail.com>
2015-09-05 14:52:45 +0545

    Updated with PEP8 standard

--- grabber.py
+++ grabber.py
@@ -6,13 +6,14 @@
 #######################################################################
 
 import urllib.request
-from urllib.error import HTTPError,URLError
+from urllib.error import HTTPError, URLError
 
-#function that downloads a file
-def downloadFile(file_name,file_mode,base_url):
-    #create the url
+
+# Function that downloads a file
+def downloadFile(file_name, file_mode, base_url):
+    # Create the url
     url = base_url + file_name + "/picture"
-   
+
     # Open the url
     try:
             f = urllib.request.urlopen(url)
@@ -23,23 +24,23 @@ def downloadFile(file_name,file_mode,base_url):
             local_file.write(f.read())
             local_file.close()
 
-    #handle errors
+    # Handle errors
     except HTTPError as e:
-            print("HTTP Error:",e.code , url)
+            print("HTTP Error:", e.code, url)
     except URLError as e:
-            print("URL Error:",e.reason , url)
+            print("URL Error:", e.reason, url)
 
-#End Function
-            
-profile=100000425733973 #Start From Profile ID          
+# End Function
+
+profile = 100000425733973  # Start From Profile ID
 
 # Iterate over image range . Infinite Loop :)
-while(1==1):   
-    base_url ='https://graph.facebook.com/'
-    s_index=str(profile)
-    file_name =s_index;
+while(1 == 1):
+    base_url = 'https://graph.facebook.com/'
+    s_index = str(profile)
+    file_name = s_index;
     # Now download the image. b for binary
-    downloadFile(file_name,"b",base_url)
-    profile=profile+1
-#End While Loop
-#End Program
+    downloadFile(file_name, "b", base_url)
+    profile = profile + 1
+# End While Loop
+# End Program
